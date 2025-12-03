42b720d17d3dd16374a4609f16632c1f9c82aebd
Rafael Lossurdo <lossurdo@gmail.com>
2025-12-03 10:29:26 -0300

    feat: Add README and implement info endpoint in main.py

---
+++ README.md
@@ -0,0 +1,19 @@
+# Documentação
+
+## Executar no localhost
+
+```shell
+$ uv run uvicorn main:app --reload
+```
+
+## Executar no Render
+
+```shell
+$ uv run uvicorn main:app --host 0.0.0.0 --port $PORT
+```
+
+URL: `https://helix-ws-test.onrender.com/`
+
+## Repositório no GitHub
+
+`https://github.com/lossurdo/helix-ws-test`
--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-# main.py
-
 from fastapi import FastAPI
 
-# 1. Inicializa o aplicativo FastAPI
 app = FastAPI()
 
-# 2. Define um endpoint GET na raiz ("/")
+
 @app.get("/")
 def read_root():
-    # Retorna um dicionário que o FastAPI converte em JSON
     return {"message": "Olá! Sua API FastAPI está rodando no Render! 🎉"}
 
-# 3. Define um endpoint com um parâmetro de caminho
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id, "description": "Este é um item de exemplo."}
 
-# Para rodar localmente, você usaria: uvicorn main:app --reload
+@app.get("/rest/info")
+def info():
+    return {
+        "message": "ok"
+    }
